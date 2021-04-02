74c70922f2af104418834618cb33ddd0bd9b0625
maryamzain123 <smaryam.zain@gmail.com>
2021-04-02 06:58:06 -0500

    commit tree 1c4cf168b94b0b55483e6431b84b7477982117e1

--- app/models/show_controller.rb
+++ app/models/show_controller.rb
@@ -1,27 +1,26 @@
 def Show.highest_rating
-   Show.maximum("rating")
-end
-
-def Show.most_popular_show
-    Show.where(rating = highest_rating)
-end
-
-def Show.lowest_rating
-    Show.minimum("rating")
-end
-def Show.least_popular_show
-   
-end
-
-def Show.ratings_sum
-    Show.sum("rating")
-end
-
-def Show.popular_shows
-    Show.where("rating > '5'")
-end
-
-def Show.shows_by_alphabetical_order
-    Show.order('name ASC')
-end
-
+    Show.maximum("rating")
+ end
+ 
+ def Show.most_popular_show
+     Show.where("rating = ?", highest_rating).first
+ end
+ 
+ def Show.lowest_rating
+     Show.minimum("rating")
+ end
+ def Show.least_popular_show
+    Show.where("rating = ?", lowest_rating).first
+ end
+ 
+ def Show.ratings_sum
+     Show.sum("rating")
+ end
+ 
+ def Show.popular_shows
+     Show.where("rating > '5'")
+ end
+ 
+ def Show.shows_by_alphabetical_order
+     Show.order('name ASC')
+ end
